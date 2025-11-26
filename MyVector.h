0c3d6c06c659b3487a55c8d2c3d1840d3efd85dd
unknown <seongrobot@gmail.com>
2025-11-26 15:55:42 +0900

    과제 제출

--- MyVector.h
+++ MyVector.h
@@ -3,7 +3,6 @@
 #include <initializer_list>
 #include <cstddef>
 
-
 template<typename T>
 class MyVector {
     T* data;            // 동적 배열을 가리키는 포인터
@@ -90,3 +89,193 @@ public:
     bool empty() const ;    // 현재 vector가 empty인지 확인하는 함수 구현
 
 };
+
+#include <stdexcept>
+
+template<typename T>
+void MyVector<T>::push_back(const T& val) {
+    if(size() == cap) {
+        size_t newCap = (cap == 0) ? 1 : cap * 2;
+        T* newData = new T[newCap];
+
+        for (size_t i = 0; i < size(); ++i) {
+            newData[i] = data[i];
+        }
+
+        delete[] data;
+        data = newData;
+        cap = newCap;
+    }
+    data[length++] = val;
+}
+template<typename T>
+void MyVector<T>::pop_back() {
+    if(length == 0) throw std::out_of_range("Index out of range");
+    length--;
+    
+}
+template<typename T>
+size_t MyVector<T>::size() const {
+    return length;
+}
+template<typename T>
+T& MyVector<T>::operator[](int i) {
+    return data[i];
+}
+
+template<typename T>
+T& MyVector<T>::Iterator::operator*() {
+    return *ptr;
+}
+template<typename T>
+typename MyVector<T>::Iterator& MyVector<T>::Iterator::operator++() {
+    ptr++;
+    return *this;
+}
+template<typename T>
+typename MyVector<T>::Iterator& MyVector<T>::Iterator::operator--() {
+    ptr--;
+    return *this;
+}
+template<typename T>
+typename MyVector<T>::Iterator MyVector<T>::Iterator::operator+(int n) const {
+    return Iterator(ptr + n);
+}
+template<typename T>
+typename MyVector<T>::Iterator MyVector<T>::Iterator::operator-(int n) const {
+    return Iterator(ptr - n);
+}
+
+template<typename T>
+typename MyVector<T>::Iterator MyVector<T>::begin() {
+    return Iterator(data);
+}
+template<typename T>
+typename MyVector<T>::Iterator MyVector<T>::end() {
+    return Iterator(data+size());
+}
+
+template<typename T>
+typename MyVector<T>::Iterator MyVector<T>::insert(Iterator pos, const T& value) {
+    long long index = pos - begin();
+
+    if (length == cap) {
+        size_t newCap = (cap == 0) ? 1 : cap * 2;
+        T* newData = new T[newCap];
+
+        for (size_t i = 0; i < length; ++i) {
+            newData[i] = data[i];
+        }
+
+        if (data) delete[] data;
+        data = newData;
+        cap = newCap; 
+    }
+
+    for (long long i = length; i > index; i--) {
+        data[i] = data[i - 1];
+    }
+
+    length++;
+    data[index] = value;
+    
+    return Iterator(data + index);
+}
+template<typename T>
+typename MyVector<T>::Iterator MyVector<T>::erase(Iterator pos) {
+    long long index = pos - begin();
+    
+    for (long long i = index; i < length - 1; i++) {
+        data[i] = data[i + 1];
+    }
+
+    length--;
+
+    return Iterator(data + index);
+}
+
+
+template<typename T>
+void MyVector<T>::clear() {
+    length = 0;
+}
+
+template<typename T>
+T& MyVector<T>::at(size_t i) {
+    if (i >= length) throw std::out_of_range("Index out of range");
+    return data[i];
+}
+
+template<typename T>
+T& MyVector<T>::front() {
+    return data[0];
+}
+template<typename T>
+T& MyVector<T>::back() {
+    return data[length-1];
+}
+
+template<typename T>
+size_t MyVector<T>::capacity() const {
+    return cap;
+} 
+
+template<typename T>
+bool MyVector<T>::empty() const {
+    return size() == 0;
+}
+
+template<typename T>
+bool MyVector<T>::operator==(const MyVector& other) const {
+    if(length != other.length) return false;
+    for(size_t i = 0; i < length; i++) {
+        if(data[i] != other.data[i]) return false;
+    }
+    return true;
+}
+
+template<typename T>
+bool MyVector<T>::operator!=(const MyVector& other) const {
+    return !(*this == other);
+}
+
+template<typename T>
+bool MyVector<T>::operator<(const MyVector& other) const {
+    size_t minLen = (length < other.length) ? length : other.length;
+
+    for (size_t i = 0; i < minLen; ++i) {
+        if (data[i] < other.data[i]) return true;  
+        if (data[i] > other.data[i]) return false; 
+    }
+    return length < other.length;
+}
+
+template<typename T>
+bool MyVector<T>::operator>(const MyVector& other) const {
+    return other < *this;
+}
+
+template<typename T>
+bool MyVector<T>::operator<=(const MyVector& other) const {
+    return !(*this > other);
+}
+
+template<typename T>
+bool MyVector<T>::operator>=(const MyVector& other) const {
+    return !(*this < other);
+}
+
+template<typename T>
+bool MyVector<T>::Iterator::operator==(const Iterator& other) const {
+    return ptr == other.ptr;
+}
+
+template<typename T>
+bool MyVector<T>::Iterator::operator!=(const Iterator& other) const {
+    return ptr != other.ptr;
+}
+
+template<typename T>
+int MyVector<T>::Iterator::operator-(const Iterator& other) const {
+    return ptr - other.ptr;
+}
